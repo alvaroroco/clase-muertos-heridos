25cea55976514c81aea1147c3bd8bdaf372914b4
alvaroroco <alvaroroco@gmail.com>
2026-02-22 13:58:08 +0100

    Init

---
+++ README.md
@@ -0,0 +1,38 @@
+# Muertos y Heridos
+
+## Descripción del juego
+
+El programa piensa un número aleatorio de 4 cifras.
+
+El usuario introduce números hasta acertarlo.
+
+Por cada intento, el programa devuelve una cadena con:
+
+M → número correcto en posición correcta (muerto)
+
+H → número correcto en posición incorrecta (herido)
+
+V → número que no está en el número pensado (vivo)
+
+Ejemplo de salida: MMHV
+
+## Reglas de entrada
+
+- El usuario debe introducir exactamente 4 dígitos.
+
+- Solo se aceptan números.
+
+- Si la entrada no es válida, se vuelve a pedir.
+
+## Objetivo
+
+Adivinar el número pensado por el programa en el menor número de intentos posible.
+Al acertar, se muestra un mensaje final con el número de intentos realizados.
+
+## Extras propuestos (para ampliar el ejercicio)
+
+Permitir elegir el número de cifras del número secreto.
+
+Limitar el número máximo de intentos.
+
+Crear un ranking según número de intentos.
---
+++ main.py
@@ -0,0 +1,50 @@
+from random import randint
+
+
+def generar_num_aleatorio():
+    num_aleatorio = "".join([str(randint(0, 9)) for i in range(4)])
+    return num_aleatorio
+
+
+def pedir_numero():
+    num_introducido = ""
+    while not num_introducido.isnumeric() or len(num_introducido) != 4:
+        num_introducido = input("Por favor, introduce un número de 4 dígitos: ")
+        if not num_introducido.isnumeric() or len(num_introducido) != 4:
+            print("El número introducido no es válido")
+
+        print("\n")
+
+    return num_introducido
+
+
+def calcular_muertos_heridos(num_pensado, num_usuario):
+    if num_pensado == num_usuario:
+        return True, "MMMM"
+
+    muertos_heridos = ["V", "V", "V", "V"]
+    for i in range(4):
+        if num_pensado[i] == num_usuario[i]:
+            muertos_heridos[i] = "M"
+        elif num_usuario[i] in num_pensado[:i] + num_pensado[i + 1 :]:
+            muertos_heridos[i] = "H"
+        else:
+            muertos_heridos[i] = "V"
+
+    return False, "".join(muertos_heridos)
+
+
+if __name__ == "__main__":
+    intentos = 0
+    ganador = False
+    num_pensado = generar_num_aleatorio()
+
+    while not ganador:
+        intentos += 1
+        num_usuario = pedir_numero()
+        ganador, muertos_heridos = calcular_muertos_heridos(num_pensado, num_usuario)
+
+        if not ganador:
+            print(f"¡Incorrecto!\nTus muertos y heridos son: {muertos_heridos}")
+
+    print(f"¡Enhorabuena! Lo has acertado en {intentos} intentos")
